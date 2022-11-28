120b262b666b04ed84a2028109f095a7401727e4
денис <biasha89@mail.ru>
2022-11-28 03:36:09 +0300

    Denis2.py

---
+++ Denis2.py
@@ -0,0 +1,15 @@
+1 #int
+per1 = "hello"
+per2 = "My name is Denis"
+2.123 #float
+per1 = 32.456
+per2 = 67.358608
+"Denis"#str
+per1 = "Denis"
+per2 = 7643
+True # bool
+per1 = True
+False # bool
+per2 = False
+None # пустой тип
+per1 = None
